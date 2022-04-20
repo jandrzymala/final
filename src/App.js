cc353066b6152556ed472b3b30b5acd40ac1a358
jandrzymala <jan.drzyma82@gmail.com>
2022-04-20 18:10:45 +0200

    scss added and import files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [counter, setCounter] = useState(1);
+ 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCounter(prevState => prevState + 1);
+    }, 1000)
+  }, []);
+ 
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <>
+        <h1>Welcome to the QUiZ <span>League</span></h1>
+        <h2>Twój czas to {counter}</h2>
+      </>
     </div>
   );
 }
