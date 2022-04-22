14613cf9651c788e4054b16de34dcbdba60e1578
jandrzymala <jan.drzyma82@gmail.com>
2022-04-22 15:32:10 +0200

    quiz League

---
+++ src/QuizLeague.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import QuizBody from './QuizBody'
+
+const QuizLeague = () => {
+return (
+    <div>
+        <ul>
+            <li><QuizBody title={'Stolice'}/></li>
+            <li><QuizBody title={'Rzeki'}/></li>
+            <li><QuizBody title={'Góry'}/></li>
+        </ul>
+    </div>
+        
+    
+)
+}
+
+export default QuizLeague
