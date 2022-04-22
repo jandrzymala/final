6a4b6b4121b5bb5e02cc7221557b8e7e7f7fc45f
jandrzymala <jan.drzyma82@gmail.com>
2022-04-22 15:30:42 +0200

    users management

---
+++ src/UsersManager.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react'
+import AddUser from './AddUser'
+
+function UsersManager() {
+const [data, setData] = useState(null)
+const [loading, setLoading] = useState(true)
+
+function addUser(user) {
+fetch('http://localhost:3000/users', {
+method: 'POST',
+body: JSON.stringify(user),
+headers: {
+'Content-Type': 'application/json',
+},
+})
+.then((response) => {
+return response.json()
+})
+.then((res) => {
+setData((prevState) => {
+return [...prevState, res]
+})
+})
+}
+
+useEffect(() => {
+fetch('http://localhost:3000/users')
+.then((response) => {
+return response.json()
+})
+.then((userData) => {
+setData(userData)
+setLoading(false)
+})
+}, [])
+
+if (loading) {
+return <div>loading </div>
+}
+return (
+<div>
+<AddUser addUser={addUser} />
+</div>
+)
+}
+
+export default UsersManager
