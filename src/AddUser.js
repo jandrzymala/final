47c7544e0abfbf2624171cae75453207fb3ad018
jandrzymala <jan.drzyma82@gmail.com>
2022-04-22 15:29:12 +0200

    component add user - add user to database

---
+++ src/AddUser.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+
+function AddUser({ addUser }) {
+const [name, setName] = useState('')
+const [totalScore, setTotalScore] = useState(0);
+
+function onSubmit(e) {
+e.preventDefault()
+
+addUser({
+name,
+totalScore: 0,
+})
+}
+
+return (
+<form onSubmit={onSubmit}>
+    <div>
+    <label>Przedstaw się, aby dołączyć</label>
+    <input
+    type='text'
+    value={name}
+    onChange={(e) => setName(e.target.value)}
+    />
+    </div>
+    <button type='submit'>DODAJ</button>
+</form>
+)
+}
+
+export default AddUser
