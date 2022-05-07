8c19596b832af3f127188ca1981b45664611debc
jandrzymala <jan.drzyma82@gmail.com>
2022-05-07 09:24:56 +0200

    header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import UsersManager from "../UsersManager/UsersManager";
+import './header.scss'
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Quizowa liga</h1>
+            <UsersManager />
+          </header>
+    )
+}
+export default Header
