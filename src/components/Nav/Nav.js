5a0779e44c5ab9c185914945e32798d2988ce688
jandrzymala <jan.drzyma82@gmail.com>
2022-05-07 09:25:23 +0200

    nav component

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './nav.scss'
+const Nav = () => {
+    return (
+        <nav>
+            <div>
+              <ul>
+                <li><a>Lista uczestników</a></li>
+              </ul>
+            </div> 
+          </nav>
+    )
+}
+export default Nav
