405fd6df5330843ce0a067cc48a94e21f4066dd4
jan.drzymala <jan.drzymala82@gmail.com>
2022-05-20 06:43:05 +0200

    corrections: adduser, singlequizbody, quizleague and quizbody styles

--- src/components/AddUser/AddUser.js
+++ src/components/AddUser/AddUser.js
@@ -1,13 +1,8 @@
 import React, { useState } from 'react'
 
-<<<<<<< HEAD
-function AddUser({ name, addUser }) {
-const [name, setName] = useState('')
-=======
 function AddUser({ addUser }) {
 const [name, setName] = useState('');
 
->>>>>>> 611cd73470754809d73766be3f0c558f83100cb5
 
 
 function onSubmit(e) {
@@ -32,41 +27,7 @@ return (
     </div>
     <button type='submit'>DODAJ</button>
 </form>
-<<<<<<< HEAD
-<h2>Witaj {name}</h2>
-const UsersList = () => {
-    const [data, setData] = useState(null)
-    const [loading, setLoading] = useState(true)
-    useEffect(() => {
-        fetch('http://localhost:3000/users')
-        .then((response) => {
-        return response.json()
-        })
-        .then((userData) => {
-        setData(userData)
-        setLoading(false)
-        })
-        }, [])
-        
-        if (loading) {
-        return <div>loading </div>
-        }
-        return (
-            <section className='users'>
-            <h3>ZAWODNICY</h3>
-            <ul className='usersList'>
-                {data.map((user) => {
-                return <SingleUser user={user} key={user.id} />
-                })}
-            </ul>
-            </section>
-        )
-}
 </>
-
-=======
-</>
->>>>>>> 611cd73470754809d73766be3f0c558f83100cb5
 )
 }
 
