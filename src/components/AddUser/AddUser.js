0ca77ed3d8157752b93a4cace8c5929470cb359d
jandrzymala <jan.drzyma82@gmail.com>
2022-05-23 08:32:17 +0200

    new procedure of adding user - step 2 - POST

--- src/components/AddUser/AddUser.js
+++ src/components/AddUser/AddUser.js
@@ -1,34 +1,42 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+const AddUser = ({ addUser }) => {
+    const [name, setName] = useState('');
+    const [show, setShow] = useState(false);
 
-function AddUser({ addUser }) {
-const [name, setName] = useState('');
-
-
-
-function onSubmit(e) {
-e.preventDefault()
-
-addUser({
-name,
-totalScore: 0,
-})
-
-}
-
-return (
-    <><form onSubmit={onSubmit}>
-    <div>
-    <label>Przedstaw się, aby dołączyć</label>
-    <input
-    type='text'
-    value={name}
-    onChange={(e) => setName(e.target.value)}
-    />
-    </div>
-    <button type='submit'>DODAJ</button>
-</form>
-</>
-)
-}
-
+    const handleSubmit = (e) => {
+        e.preventDefault(); 
+    }
+ 
+    const handleClick = (e) => {
+     setShow(true);
+    }
+    function onSubmit(e) {
+        e.preventDefault()
+        
+        addUser({
+        name,
+        totalScore: 0,
+        })
+        
+        }
+    return (
+        <>
+        { !show && (
+            <>
+            <div> 
+            <form onSubmit={handleSubmit}>
+                <label>Przedstaw się</label>
+                <input
+                    type='text'
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                />
+                <button type='submit' onClick={handleClick}>Wyślij</button>
+            </form></div></>
+            )
+        }
+{show && (<><form onSubmit={onSubmit}><label>Witaj {name}</label><button type='submit'>Dołącz!</button></form></>)}
+        
+        </>
+    )}
 export default AddUser
