05720b39700dc85aee932d0349b3221084125cb5
jandrzymala <jan.drzyma82@gmail.com>
2022-05-23 13:24:52 +0200

    update QuizLeague

--- src/components/QuizLeague/QuizLeague.js
+++ src/components/QuizLeague/QuizLeague.js
@@ -1,24 +1,19 @@
 import React, {Component} from "react";
-import SingleQuizBody1 from "../Quizes/SingleQuizBody1";
-import SingleQuizBody2 from "../Quizes/SingleQuizBody2";
-import SingleQuizBody3 from "../Quizes/SingleQuizBody3";
 import QuizBody from '../FullQuizBody/QuizBody'
 import questions from '../Quizes/quiz1' 
 import questions2 from '../Quizes/quiz2' 
 import questions3 from '../Quizes/quiz3' 
 import SingleQuizBody from '../Quizes/SingleQuizBody';
-import UsersList from "../UsersList/UsersList";
 
-const QuizLeague = ({userName}) => {
+const QuizLeague = () => {
 return (
     <div>
         <ul>
-            <li><QuizBody title={'Stolice'} singleQuiz={<SingleQuizBody1 />}/></li>
-            <li><QuizBody title={'Rzeki'} singleQuiz={<SingleQuizBody2 />} questionsData={questions}/></li>
-            <li><QuizBody title={'Góry'} singleQuiz={<SingleQuizBody3 />}/></li>
-            <li><QuizBody title={'Różne'} singleQuiz={<SingleQuizBody quiz={questions3}/>} /></li>
+            <li><QuizBody title={'Stolice'} singleQuiz={<SingleQuizBody quiz={questions}/>}/></li>
+            <li><QuizBody title={'Rzeki'} singleQuiz={<SingleQuizBody quiz={questions2}/>} questionsData={questions}/></li>
+            <li><QuizBody title={'Góry'} singleQuiz={<SingleQuizBody quiz={questions3}/>}/></li>
+            <li><QuizBody title={'Różne'} singleQuiz={<SingleQuizBody quiz={questions}/>} /></li>
         </ul>
-        <UsersList />
     </div>
         
     
