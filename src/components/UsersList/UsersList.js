7c45f8f541f951e4c403db8bd57cd3a19bec660f
jan.drzymala <jan.drzymala82@gmail.com>
2022-05-25 05:58:49 +0200

    unnecessary function savetolocalstorage removed

--- src/components/UsersList/UsersList.js
+++ src/components/UsersList/UsersList.js
@@ -1,37 +1,36 @@
-import { useState, useEffect } from 'react';
-import SingleUser from '../SingleUser/SingleUser';
-import './userslist.scss'
-const saveToLocalstorage=()=> {
-    let users = fetch('http://localhost:3000/users');
-    console.log(users);
-    localStorage.setItem('current-user-id', users.id)
-}
+import { useState, useEffect } from "react";
+
+import "./userslist.scss";
 const UsersList = () => {
-    const [data, setData] = useState(null)
-    const [loading, setLoading] = useState(true)
-    useEffect(() => {
-        fetch('http://localhost:3000/users')
-        .then((response) => {
-        return response.json()
-        })
-        .then((userData) => {
-        setData(userData)
-        setLoading(false)
-        })
-        }, [])
-        
-        if (loading) {
-        return <div>loading </div>
-        }
-        return (
-            <section className='users'>
-            <h3>Wybierz zawodnika</h3>
-            <select className='usersList' onChange={saveToLocalstorage}>
-                {data.map((user) => {
-                return <option user={user} key={user.id}>{user.name}</option>
-                })}
-            </select>
-            </section>
-        )
-}
-export default UsersList
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    fetch("http://localhost:3000/users")
+      .then((response) => {
+        return response.json();
+      })
+      .then((userData) => {
+        setData(userData);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <div>loading </div>;
+  }
+  return (
+    <section className="users">
+      <h3>Wybierz zawodnika</h3>
+      <select className="usersList">
+        {data.map((user) => {
+          return (
+            <option user={user} key={user.id}>
+              {user.name}
+            </option>
+          );
+        })}
+      </select>
+    </section>
+  );
+};
+export default UsersList;
