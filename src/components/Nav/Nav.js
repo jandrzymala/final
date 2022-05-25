3a474baae1e9505760fb810a7ef6b0ce984eaa6f
jan.drzymala <jan.drzymala82@gmail.com>
2022-05-25 05:56:27 +0200

    preventing warnings in nav file

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,21 +1,23 @@
-import React, {useState} from "react";
-import UsersList from "../UsersList/UsersList";
+import React, { useState } from "react";
 import UsersManager from "../UsersManager/UsersManager";
-import './nav.scss'
+import "./nav.scss";
 const Nav = () => {
   const [show, setShow] = useState(false);
   const handleClick = (e) => {
-         setShow(!show);
-        }
+    setShow(!show);
+  };
 
-    return (
-        <nav>
-            <div>
-              <ul>
-                <li><a onClick={handleClick}>Lista uczestników</a>{ show && (<UsersManager />)}</li>
-              </ul>
-            </div> 
-          </nav>
-    )
-}
-export default Nav
+  return (
+    <nav>
+      <div>
+        <ul>
+          <li>
+            <span onClick={handleClick}>Lista uczestników</span>
+            {show && <UsersManager />}
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+export default Nav;
