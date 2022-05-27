ca86d22fe3586339c3102fe3e1e552b328b70cd4
jandrzymala <jan.drzyma82@gmail.com>
2022-05-27 17:07:56 +0200

    userslist updated

--- src/components/UsersList/UsersList.js
+++ src/components/UsersList/UsersList.js
@@ -1,9 +1,15 @@
 import { useState, useEffect } from "react";
-
+import SingleUser from "../SingleUser/SingleUser";
 import "./userslist.scss";
 const UsersList = () => {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [selected, setSelected] = useState(null)
+  const saveToLocalStorage = (name, totalScore, id) => {
+      localStorage.setItem("current-user-name", name);
+      localStorage.setItem("current-user-totalScore", totalScore);
+      localStorage.setItem("current-user-id", id);
+  }
   useEffect(() => {
     fetch("http://localhost:3000/users")
       .then((response) => {
@@ -15,17 +21,27 @@ const UsersList = () => {
       });
   }, []);
 
+  useEffect(() => {
+    if(selected) {
+      console.log(data[selected]);
+      const {name, totalScore, id} = data[selected];
+      saveToLocalStorage(name, totalScore, id);
+    }
+  }, [selected])
+
   if (loading) {
     return <div>loading </div>;
   }
+
   return (
     <section className="users">
       <h3>Wybierz zawodnika</h3>
-      <select className="usersList">
-        {data.map((user) => {
+      <select className="usersList" value={selected} onChange={e => setSelected(e.target.value)}>
+        <option value={null}>Wybierz...</option>
+        {data.map((user, idx) => {
           return (
-            <option user={user} key={user.id}>
-              {user.name}
+            <option user={user} key={user.id} value={idx}>
+             {user.id}. Nazwa: {user.name}. Wynik: {user.totalScore} 
             </option>
           );
         })}
